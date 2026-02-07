3b962b59ec089d1e4c060bdea6cedd32ca7a971d
unknown <salviagui@gmail.com>
2026-02-07 18:47:19 -0300

    angular packages added, taiga ui installed ✨

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,18 +1,25 @@
 import {
   ApplicationConfig,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
+  BrowserModule,
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {provideEventPlugins} from '@taiga-ui/event-plugins';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(BrowserModule),
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
+    provideEventPlugins(),
+    provideAnimations(),
     provideRouter(appRoutes),
   ],
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
-
+import { TuiRoot, TuiButton } from '@taiga-ui/core';
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, TuiRoot, TuiButton],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
