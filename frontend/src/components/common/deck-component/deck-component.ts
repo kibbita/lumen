bdb6675780793614414901a29efe3be56406b64c
unknown <salviagui@gmail.com>
2026-03-07 14:41:09 -0300

    search signals deck list✨

--- frontend/src/components/common/deck-component/deck-component.ts
+++ frontend/src/components/common/deck-component/deck-component.ts
@@ -2,7 +2,6 @@ import { Component, input, InputSignal } from '@angular/core';
 import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 import { DeckGetDto } from '../../../models/deckGetDto';
 import { TuiChip } from '@taiga-ui/kit';
-import { TuiAutoColorPipe } from '@taiga-ui/core';
 @Component({
   selector: 'app-deck-component',
   imports: [TuiCardLarge, TuiHeader, TuiChip],
